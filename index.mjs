47e4aa6a53972c0c3faa97e2d1550efb8405c8b2
discodevl <donato.cutro3@gmail.com>
2022-04-03 01:44:22 +0200

    change interval

--- index.mjs
+++ index.mjs
@@ -3,27 +3,38 @@ import cron from "node-cron";
 import { templates } from "./data/getTemplate.mjs";
 import { subjects } from "./data/getSubject.mjs";
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 async function generateTweet() {
+  const shuffledSubjects = shuffleArray(subjects);
+  const shuffledTemplates = shuffleArray(templates);
+
   const subjectIndex = Math.floor(Math.random() * subjects.length);
-  const subject = subjects[subjectIndex];
+  const subject = shuffledSubjects[subjectIndex];
   const templateIndex = Math.floor(Math.random() * templates.length);
-  const template = templates[templateIndex];
-  
+  const template = shuffledTemplates[templateIndex];
+
   let result = template.replace("@", subject);
   return result;
-
 }
 
 async function tweet() {
   try {
     const tweet = await generateTweet();
+    console.log({ tweet });
     await rwClient.v1.tweet(tweet);
   } catch (error) {
     console.log(error.message);
   }
 }
-console.log({ templates });
-console.log({ subjects });
 
 function main() {
   cron.schedule("*/25 * * * *", () => {
